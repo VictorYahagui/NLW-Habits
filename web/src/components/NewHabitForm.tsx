0eec31637dfabda78c8370f57fcb33102282802b
Victor Yahagui <victoryahagui@gmail.com>
2023-01-27 16:31:59 -0300

    finish front-end api integration and focus adaptation

--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -41,7 +41,7 @@ export function NewHabitFrom() {
                 type="text"
                 id="title"
                 placeholder="Ex:Dormi, exercicios, etc..."
-                className="p-4 rounded-lg mt-3 bg-zinc-800 text-white hover:text-zinc-400"
+                className="p-4 rounded-lg mt-3 bg-zinc-800 text-white hover:text-zinc-400 focus:outline-none focus:right-2 focus:ring-violet-800 focus:ring-offset-2 focus:ring-offset-zinc-900"
                 autoFocus
                 value={title}
                 onChange={event => { setTitle(event.target.value) }}
@@ -56,11 +56,11 @@ export function NewHabitFrom() {
                     return (
                         <Checkbox.Root
                             key={weekDay}
-                            className='flex items-center gap-3 group'
+                            className='flex items-center gap-3 group focus:outline-none'
                             checked={weekDays.includes(index)}
                             onCheckedChange={() => { handleToggleWeekDay(index) }}
                         >
-                            <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
+                            <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500 transition-colors group-focus:right-2 group-focus:ring-violet-800 group-focus:ring-offset-2 group-focus:ring-offset-background'>
                                 <Checkbox.Indicator>
                                     <Check size={20} className="text-white" />
                                 </Checkbox.Indicator>
@@ -76,7 +76,7 @@ export function NewHabitFrom() {
 
             <button
                 type="submit"
-                className="mt-3 rounded-lg p-4 flex items-center gap-3 font-semibold justify-center bg-green-600 hover:bg-green-500"
+                className="mt-3 rounded-lg p-4 flex items-center gap-3 font-semibold justify-center bg-green-600 hover:bg-green-500 transition-colors focus:outline-none focus:right-2 focus:ring-green-600 focus:ring-offset-2 focus:ring-offset-zinc-900"
             >
                 <Check size={20} weight='bold' />
                 confirm
